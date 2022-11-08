42b98b534ab5e2bfc94369a19c3002353c3c392d
Diogo Silva <dasilva@academiafa.edu.pt>
2022-11-08 15:55:12 +0000

    notebook like main.py (scientific mode of PyCharm)

---
+++ main.py
@@ -0,0 +1,153 @@
+#%% create and print variable
+a = 1
+print(a)
+
+
+#%% how print works
+print('value of a=', a)
+
+#%% what are types
+print('type of a=', type(a))
+
+#%% what other types are there
+a = 3.14
+print('value of a=', a)
+print('type of a=', type(a))
+
+#%% let's get input from the user
+a = input('insert value for a:')
+print('value of a=', a)
+print('type of a=', type(a))
+
+#%% convert string to number
+a = int(a)
+print('value of a=', a)
+print('type of a=', type(a))
+
+#%% let's convert the str to a type we can parse
+a = float(a)
+print('value of a=', a)
+print('type of a=', type(a))
+
+#%% what about lists?
+lst = [1, 2, 3, 4, 5, 6]
+print('lst=', lst)
+print('type of lst=', type(lst))
+
+
+#%% what is the size of the list?
+
+lst[0] = a
+print(lst)
+print('lst=', lst)
+print('size of lst=', len(lst))
+
+#%% can we add values to the list?
+
+lst.append(42)
+print(lst)
+
+#%% how to remote? if we know a specific element, we can just
+lst.remove(42)
+print(lst)
+
+#%% or we can remove the element on a particular index
+lst.pop(0)
+print(lst)
+
+#%% how can we go through every value of a list? we can use cycles
+for i in range(len(lst)):
+    print(lst[i])
+
+#%% we could also change the values of the list
+print(lst)
+for i in range(len(lst)):
+    lst[i] = lst[i]*2
+print(lst)
+
+
+
+#%% what if we wanted to print each element of the list with 1 second delay?
+# we need to import outside modules
+import time
+
+
+for i in range(len(lst)):
+    print(lst[i])
+    time.sleep(1)
+
+#%% let's also know when we printed each value
+import datetime
+
+for i in range(len(lst)):
+    print(datetime.datetime.now(), lst[i])
+    time.sleep(1)
+
+#%% how to store data outside the program? we use files
+with open('test.txt', 'w') as f:
+    f.write('hello world!')
+
+#%% how to read it?
+with open('test.txt', 'r') as f:
+    print(f.read())
+
+
+#%% files: error when not writing str for text files
+with open('test.txt', 'w') as f:
+    f.write(lst)
+
+#%% convert to string and write
+with open('test.txt', 'w') as f:
+    f.write(str(lst))
+
+
+#%% read from file
+with open('test.txt', 'r') as f:
+    lst2 = f.read()
+
+print(lst2)
+
+#%% we have a string, so we need to parse the content, let's try to directly convert it to a list
+lst2_ = list(lst2)
+print(lst2_)
+
+#%% didn't work so well, let's remove the square brackets
+
+lst2_ = lst2[1:-1]
+print(lst2_)
+#%% now let's split the values
+lst2_ = lst2[1:-1].split(',')
+print(lst2_)
+
+#%% now let's conver the strings to real numbers
+lst3 = []
+for i in range(len(lst2_)):
+    val = float(lst2_[i])
+    lst3.append(val)
+print(lst3)
+
+
+#%% write in a format that can be easily parsed after reading
+
+
+
+
+#%% Install dronekit
+# git clone https://github.com/dronekit/dronekit-python
+# pip install /path/to/dronekit-python
+
+#%%
+import dronekit
+
+
+#%%
+vehicle = dronekit.connect('/dev/ttyACM0', wait_ready=True, baud=57600)
+
+#%%
+
+import time
+
+while True:
+    att = vehicle.attitude
+    print(att)
+    time.sleep(1)
